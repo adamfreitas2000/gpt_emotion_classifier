27aef51986c028895dbc2d8c9b5cb28e0307eacf
Adam Perez <adamfreitas2000@gmail.com>
2025-05-25 10:49:39 -0300

    changing models

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import requests
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
+GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemma-3n-e4b-it:generateContent?key={GEMINI_API_KEY}"
 
 app = FastAPI()
 
